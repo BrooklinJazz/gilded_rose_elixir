abb76d672b6ee26263e71fe87ea9d607fd43677e
Axel Clark <axel.clark@pm.me>
2023-07-19 09:27:59 -0700

    Add initial tests for normal items

--- test/gilded_rose_test.exs
+++ test/gilded_rose_test.exs
@@ -1,10 +1,29 @@
 defmodule GildedRoseTest do
   use ExUnit.Case
 
-  test "begin the journey of refactoring" do
-    items = [%Item{name: "foo", sell_in: 0, quality: 0}]
-    GildedRose.update_quality(items)
-    %{name: firstItemName} = List.first(items)
-    assert "fixme" == firstItemName
+  # system under test
+  # context of the system (state)
+  # behavior (optional as it's contained in the assertion)
+
+  describe "update_items/1" do
+    test "normal item _ decrement sell_in and quality" do
+      items = [%Item{name: "foo", sell_in: 30, quality: 50}]
+      assert [item] = GildedRose.update_quality(items)
+      assert item.quality == 49
+      assert item.sell_in == 29
+    end
+
+    test "degrades normal item twice as fast after sell by date" do
+      items = [%Item{name: "foo", sell_in: 0, quality: 50}]
+      assert [item] = GildedRose.update_quality(items)
+      assert item.quality == 48
+      assert item.sell_in == -1
+    end
+
+    test "quality of a normal item is never negative" do
+      items = [%Item{name: "foo", sell_in: 30, quality: 0}]
+      assert [item] = GildedRose.update_quality(items)
+      assert item.quality == 0
+    end
   end
 end
